4ed7291c2a6d3c65961c50acda05175ae0fa8204
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2023-11-15 15:30:38 +0000

    N

--- Time taken to print index/timeTaken.sh
+++ Time taken to print index/timeTaken.sh
@@ -1,4 +1,4 @@
-timeTaken() {
+timeTakenFailed() {
     # print separator
     echo "----------------------------------------"
 
@@ -35,6 +35,48 @@ timeTaken() {
     echo "Time difference between printing and not printing for $index entries: $combined seconds"
 }
 
+#timeTakenFailed 50000
+
 # Note that | bc seems not to be working as intended so I will try to experiment with it
 
-timeTaken 500000
+bcTest(){
+    # print separator
+    echo "----------------------------------------"
+    echo "Testing bc"
+    echo "Output of 5/2 is:"
+    # try to the result of a calculation that will result in a decimal number
+    echo "scale=5; 5/2" | bc -l
+    # This works as 2.50000
+    # lets try variables now
+    result=$(echo "scale=2; 5/2" | bc -l)
+    echo "$result"
+    # This works as 2.50
+
+}
+
+#bcTest
+
+timeTaken() {
+    # print separator
+    echo "----------------------------------------"
+    # ok, lets try to get one time to work
+    local max=$1
+    start_time=$(date +%s%3N)
+    index=0
+    while [ $index -le $max ]; do
+        echo $index
+        ((index++))
+    done
+    end_time=$(date +%s%3N)
+
+    elapsed_time=$( { time -p your_command_here > /dev/null; } 2>&1 | awk '/real/ {print $2}' )
+
+    elapsed_time_ms=$(echo "scale=3; $elapsed_time * 1000" | bc)
+    
+    echo "Elapsed time: $elapsed_time_ms milliseconds"
+    # print output
+    #echo "Time taken to print $index entries: $elapsed_time1 seconds"
+}
+
+timeTaken 50000
+
